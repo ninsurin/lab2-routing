272017c6ee7dc5268e62c71a82691a7df9900c75
ninsurin <85234584+ninsurin@users.noreply.github.com>
2021-06-25 16:51:25 +0700

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BackendService } from './backend.service';
+import { HomeComponent } from './home/home.component';
+import { CatalogModule } from './catalog/catalog.module';
+import { HomeModule } from './home/home.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    
+  ],
+  imports: [
+    BrowserModule,CatalogModule,HomeModule
+  ],
+  providers: [BackendService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
